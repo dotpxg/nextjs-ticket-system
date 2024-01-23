a5479dd336ebdf9150524637534be42f57b17911
dotpxg <dotpxg@gmail.com>
2024-01-23 19:43:55 +0530

    Add css styles and font changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link"
+import Image from "next/image"
+import Logo from "@/public/next.svg"
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Vision Helpdesk"
+        width={70}
+        quality={100}
+      />
       <h1>Vision Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
